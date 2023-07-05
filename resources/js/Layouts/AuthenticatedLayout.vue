42f41eb6122219d5b78ade7a970e0c7409f52812
permadihendra <26792223+permadihendra@users.noreply.github.com>
2023-07-05 20:19:50 +0700

    Fix User profile

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,7 +33,7 @@ const showingNavigationDropdown = ref(false);
                                 <div
                                     class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
                                 >
-                                    <NavLink :href="route('dashboard')">
+                                    <NavLink>
                                         <h1
                                             class="font-bold text-lg text-grey-900"
                                         >
@@ -234,6 +234,36 @@ const showingNavigationDropdown = ref(false);
                                     </SidebarLink>
 
                                     <SidebarLink
+                                        href="#"
+                                        :active="$page.url.startsWith('#')"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="w-5 h-5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
+                                            />
+                                        </svg>
+                                        <span class="mx-2 text-sm font-medium"
+                                            >Data Input</span
+                                        >
+                                    </SidebarLink>
+                                </div>
+
+                                <div class="space-y-3">
+                                    <label
+                                        class="px-3 text-xs text-gray-200 font-bold uppercase"
+                                        >Admin Menu</label
+                                    >
+
+                                    <SidebarLink
                                         href="/users"
                                         :active="$page.url.startsWith('/users')"
                                     >
@@ -282,7 +312,7 @@ const showingNavigationDropdown = ref(false);
             >
                 © 2023
                 <a class="text-neutral-800 dark:text-neutral-200" href="#"
-                    >permadiworks v1.1</a
+                    >permadiworks v1.2</a
                 >
             </div>
         </div>
